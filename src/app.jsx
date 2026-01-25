2fe971d8aa153ff0cfaab3a2078967600c072e1b
Jaxon Ludtke <jaxon.ludtke1@gmail.com>
2026-01-24 18:42:57 -0700

    Ran the npm run dev, but ran the debugger and it showed I had an error in app.jsx, because I didn't import react. Added that to my app.jsx.

--- src/app.jsx
+++ src/app.jsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import Login from './login/login.jsx'
@@ -15,3 +16,5 @@ export default function App() {
     </BrowserRouter>
   )
 }
+
+
