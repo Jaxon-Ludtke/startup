68e26c8a4a5ab6ba4734024b5559b31309a82cba
Jaxon Ludtke <jaxon.ludtke1@gmail.com>
2026-01-24 20:06:11 -0700

    Copied and pasted calculate.html over, changed class to className, included Links, imported react, etc. Did the same thing here as I did login.jsx and scenarios.jsx.

--- src/calculate/calculate.jsx
+++ src/calculate/calculate.jsx
@@ -1,9 +1,113 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
 export default function Calculate() {
   return (
     <>
-      <h1>Calculate</h1>
-      <p>Calculate page (React version)</p>
+      <header>
+        <h1>DealFlow</h1>
+
+        <nav>
+          <menu>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/scenarios">Scenarios</Link></li>
+            <li><Link to="/calculate">Calculate</Link></li>
+          </menu>
+        </nav>
+
+        <h4>
+          User: <span className="username">jpludtke</span>
+        </h4>
+        <hr />
+      </header>
+
+      <main className="container">
+        <form>
+          <div>
+            <label htmlFor="cashflow">💵 What is your annual cash flow?</label>
+            <input
+              type="number"
+              id="cashflow"
+              name="cashflow"
+              placeholder="4000"
+            />
+          </div>
+
+          <div>
+            <label htmlFor="cashinvested">📈 What is your cash invested?</label>
+            <input
+              type="number"
+              id="cashinvested"
+              name="cashinvested"
+              placeholder="35000"
+            />
+          </div>
+
+          <button type="submit" className="btn btn-primary">
+            Calculate
+          </button>
+
+          <div>
+            <label htmlFor="results">Cash on Cash Return:</label>
+            <input
+              type="number"
+              id="results"
+              placeholder="11.4"
+              readOnly
+            />
+          </div>
+        </form>
+
+        <hr />
+
+        <div>
+          <h3>Save This Scenario</h3>
+
+          <label htmlFor="scenario-name">Scenario Name</label>
+          <br />
+          <input
+            type="text"
+            id="scenario-name"
+            name="scenarioName"
+            placeholder="Provo Duplex, 20% down"
+          />
+          <br /><br />
+
+          <button type="button" className="btn btn-secondary">
+            Save Scenario
+          </button>
+        </div>
+
+        <hr />
+
+        <div>
+          <h4>Live Feed</h4>
+          <ul>
+            <li className="player name">
+              John Doe submitted a deal... 5.5% CoC return!
+            </li>
+            <li className="player name">
+              Jack Smith submitted a deal... 3.3% CoC return!
+            </li>
+            <li className="player name">
+              Johnny Test submitted a deal... 3.1% CoC return!
+            </li>
+          </ul>
+        </div>
+
+        <div>
+          <h4>Live Interest Rate Tracker</h4>
+          <p>Bond and/or mortgage interest rates today: 5.5%</p>
+        </div>
+      </main>
+
+      <footer>
+        <hr />
+        <span className="text-reset">Jaxon Ludtke</span>
+        <br />
+        <a href="https://github.com/Jaxon-Ludtke/startup.git">GitHub</a>
+      </footer>
     </>
   )
 }
+
