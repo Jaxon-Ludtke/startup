747b1b4dd2064037ee92e22c04f432279d09d0a1
Jaxon Ludtke <jaxon.ludtke1@gmail.com>
2026-01-28 20:45:50 -0700

    Added helpers to calculate.jsx as well for logout feature

--- src/calculate/calculate.jsx
+++ src/calculate/calculate.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { clearUser, getUser } from "../auth";
 
 export default function Calculate() {
   return (
