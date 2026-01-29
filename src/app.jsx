d1cc74f80855d604a50f5b47fe8bb2d6e38e4d80
Jaxon Ludtke <jaxon.ludtke1@gmail.com>
2026-01-28 18:28:09 -0700

    Renamed function from requirelogin to RequireLogin to be consistent, fixed case sensitivity issues in app.jsx (changed requiredlogin to RequireLogin), added RequireLogin tags to protect routes and integrate auth.js and requirelogin.js

--- src/app.jsx
+++ src/app.jsx
@@ -4,14 +4,15 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from './login/login.jsx'
 import Scenarios from './scenarios/scenarios.jsx'
 import Calculate from './calculate/calculate.jsx'
+import RequireLogin from './requirelogin.jsx'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/scenarios" element={<Scenarios />} />
-        <Route path="/calculate" element={<Calculate />} />
+        <Route path="/scenarios" element={<RequireLogin><Scenarios /></RequireLogin>} />
+        <Route path="/calculate" element={<RequireLogin><Calculate /></RequireLogin>} />
       </Routes>
     </BrowserRouter>
   )
--- src/requirelogin.js
+++ src/requirelogin.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Navigate, useLocation } from "react-router-dom";
 import { getUser } from "./auth";
 
-export default function RequireAuth({ children }) {
+export default function RequireLogin({ children }) {
   const user = getUser();
   const location = useLocation();
 
