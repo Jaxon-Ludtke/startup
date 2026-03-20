1e9bc0d752fa1108c38842f25909cec7dc46f4ce
Jaxon Ludtke <jaxon.ludtke1@gmail.com>
2026-03-19 18:18:48 -0600

    added handling of creating accounts/logging in and out - create endpoint is called when someone wants to create an account, disallow anyone who registers with an email that's already in use, bcrypt checks the password, remove the token from the user if they logout.

--- src/service/index.js
+++ src/service/index.js
@@ -18,3 +18,48 @@ app.use(express.static('public'));
 
 const apiRouter = express.Router();
 app.use('/api', apiRouter);
+
+apiRouter.post('/auth/create', async (req, res) => {
+  const existingUser = await findUser('email', req.body.email);
+
+  if (existingUser) {
+    res.status(409).send({ msg: 'That email is already taken' });
+    return;
+  }
+
+  const newUser = await createUser(req.body.email, req.body.password);
+  setAuthCookie(res, newUser.token);
+  res.send({ email: newUser.email });
+});
+
+apiRouter.post('/auth/login', async (req, res) => {
+  const user = await findUser('email', req.body.email);
+
+  if (!user) {
+    res.status(401).send({ msg: 'No account found with that email' });
+    return;
+  }
+
+  const passwordMatches = await bcrypt.compare(req.body.password, user.password);
+
+  if (!passwordMatches) {
+    res.status(401).send({ msg: 'Wrong password' });
+    return;
+  }
+
+  user.token = uuid.v4();
+  setAuthCookie(res, user.token);
+  res.send({ email: user.email });
+});
+
+apiRouter.delete('/auth/logout', async (req, res) => {
+  const user = await findUser('token', req.cookies[authCookieName]);
+
+  if (user) {
+    delete user.token;
+  }
+
+  res.clearCookie(authCookieName);
+  res.status(204).end();
+});
+
