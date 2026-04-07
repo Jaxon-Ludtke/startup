2ae444497707e50f1ccc794e45843ea587c02fcd
Jaxon Ludtke <jaxon.ludtke1@gmail.com>
2026-04-07 09:28:41 -0600

    added updateUser function to generate new token, getScenarios for fetching scenarios for given user, addScenario for savinga  new scenario in mongodb, deleteScenario to delete it, module exports as well so all these functions are aviailable to index.js.

--- service/database.js
+++ service/database.js
@@ -7,10 +7,11 @@ const config = require('./dbConfig.json');
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 
 const client = new MongoClient(url);
+const db = client.db('dealflow');
 const userCollection = db.collection('user');
 const scenarioCollection = db.collection('scenario');
 
-async function testConnection() {
+(async function testConnection() {
   try {
     await db.command({ ping: 1 });
     console.log(`DB connected to ${config.hostname}`);
@@ -18,6 +19,7 @@ async function testConnection() {
     console.log(`Connection failed to ${url} because ${ex.message}`);
     process.exit(1);
   }
+})();
 
   function getUser(email) {
   return userCollection.findOne({ email });
@@ -26,4 +28,33 @@ async function testConnection() {
 function getUserByToken(token) {
   return userCollection.findOne({ token });
 }
+
+async function createUser(user) {
+  await userCollection.insertOne(user);
+}
+
+async function updateUser(user) {
+  await userCollection.updateOne({ email: user.email }, { $set: user });
+}
+
+async function getScenarios(email) {
+  return scenarioCollection.find({ email }).sort({ _id: -1 }).toArray();
+}
+
+async function addScenario(scenario) {
+  await scenarioCollection.insertOne(scenario);
+}
+
+async function deleteScenario(id, email) {
+  return scenarioCollection.deleteOne({ id, email });
+}
+
+module.exports = {
+  getUser,
+  getUserByToken,
+  createUser,
+  updateUser,
+  getScenarios,
+  addScenario,
+  deleteScenario,
 };
